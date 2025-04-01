09a4e01a9d9244af1641b87d72c0efef36129039
VadimSamodurov1975 <75vadimalena96@gmail.com>
2025-04-01 11:17:47 +0000

    Deploying to gh-pages from @ VadimSamodurov1975/goit-js-hw-09@b8e9ed48715be59a967cc7f0eeeeb6d8d36a4717 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as e}from"./assets/vendor-CgTBfC_f.js";document.querySelector(".gallery");gallery.insertAdjacentHTML("beforeend",galleryImg);new e(".gallery a",{captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=1-gallery.js.map
